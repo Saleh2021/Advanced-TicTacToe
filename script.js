01243f5742bb0826f5a311e69ef03ae04edc3bac
sHafyane <nbsalehhafyane@gmail.com>
2021-09-16 13:33:11 +0200

    simple tictactoe coop

---
+++ script.js
@@ -0,0 +1,96 @@
+var trackBoard;
+const humPlayer = 'O'
+const aiPlayer = 'X'
+var humTurn = true
+const winArray = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [6,4,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+]
+const cells = document.querySelectorAll('.cell')
+startGame()
+function startGame(){
+    humTurn = true
+    document.querySelector('.winning-message').style.display="none"
+    document.getElementById("board").style.backgroundColor = "white"
+    trackBoard = Array.from(Array(9).keys())
+    for(let i=0;i<cells.length;i++){
+        cells[i].innerText=''
+        cells[i].style.removeProperty('background-color')
+        cells[i].style.cursor= "pointer"
+        cells[i].addEventListener('click',turnClick,{once:true})
+    }
+}
+function turnClick(square){
+    
+    if(humTurn){
+        turn(square.target.id,humPlayer);
+        humTurn = !humTurn
+    }else if(!humTurn){
+        turn(square.target.id,aiPlayer);
+        humTurn = !humTurn
+    }
+
+    
+}
+function turn(squareId,player){
+    trackBoard[squareId] = player;
+    cells[squareId].style.cursor= "not-allowed"
+    document.getElementById(squareId).innerText = player
+    let gameWon = checkWin(trackBoard,player)
+    if (gameWon) gameOver(gameWon)
+}
+//checking the winner
+function checkWin(board,player){
+    let plays = board.reduce((a,e,i)=>
+    (e===player)?a.concat(i):a,[]);
+    console.log(plays)
+    let gameWon=null
+    for(let [index,win]of winArray.entries()){
+        if(win.every(elem => plays.indexOf(elem)>-1)){
+            gameWon = {index:index,player:player}
+            break;
+        }
+    }
+    itDraw(plays,gameWon)
+    return gameWon;
+}
+// Coloring winning blocks and stop them from listening
+function gameOver(gameWon){
+    for(let index of winArray[gameWon.index]){
+        document.getElementById(index).style.backgroundColor = gameWon.player == humPlayer?"green":"red";
+    }
+    for(var i = 0; i<cells.length;i++){
+        cells[i].removeEventListener('click',turnClick,false)
+    }
+    winningMsg(gameWon.player)
+}
+//if it's a Draw
+function itDraw(plays,gameWon){
+    if(plays.length === 5 && gameWon===null){
+        for(var i = 0; i<cells.length;i++){
+            cells[i].removeEventListener('click',turnClick,false)
+        }
+        document.getElementById("board").style.backgroundColor = "yellow"
+        winningMsg("draw")
+    }
+}
+//show winning message 
+function winningMsg(player){
+    document.getElementById("msg").style.display="block"
+    if(player === humPlayer){
+        document.getElementById("txt").innerText="You Won!"
+    }else if(player === aiPlayer){
+        document.getElementById("txt").innerText="You Lost!"
+    }else if(player === "draw"){
+        document.getElementById("txt").innerText="It's a Draw!"
+    }
+}
+
+
+
